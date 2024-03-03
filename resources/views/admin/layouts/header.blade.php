e44cc5c6e24a051384cbdf6836485b7564313a09
ahmedshakib <ahmedshakib37@gmail.com>
2024-03-03 17:36:52 +0600

    Admin Dashboard add items box.

--- resources/views/admin/layouts/header.blade.php
+++ resources/views/admin/layouts/header.blade.php
@@ -150,7 +150,7 @@
 
                     <li class="nav-item">
                         <a href="{{ url('admin/admin/list') }}" class="nav-link @if(Request::segment(2) == 'admin') active @endif">
-                            <i class="nav-icon fas fa-user"></i>
+                            <i class="nav-icon fas fa-user-secret"></i>
                             <p> Admin </p>
                         </a>
                     </li>
