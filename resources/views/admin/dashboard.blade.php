8aad635f24b87e82913ed4602933c50af03de73b
ahmedshakib <ahmedshakib37@gmail.com>
2024-03-03 12:41:27 +0600

    Admin Dashboard remove cmnt.

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -278,11 +278,11 @@
                             <span class="info-box-text">Messages</span>
                             <span class="info-box-number">1,410</span>
                         </div>
-                        <!-- /.info-box-content -->
+                        
                         </div>
-                        <!-- /.info-box -->
+                        
                     </div>
-                    <!-- /.col -->
+                    
                     <div class="col-md-3 col-sm-6 col-12">
                         <div class="info-box">
                             <span class="info-box-icon bg-success"><i class="far fa-flag"></i></span>
@@ -291,11 +291,11 @@
                                 <span class="info-box-text">Bookmarks</span>
                                 <span class="info-box-number">410</span>
                             </div>
-                            <!-- /.info-box-content -->
+                            
                         </div>
-                        <!-- /.info-box -->
+                        
                     </div>
-                    <!-- /.col -->
+                    
                     <div class="col-md-3 col-sm-6 col-12">
                         <div class="info-box">
                             <span class="info-box-icon bg-warning"><i class="far fa-copy"></i></span>
@@ -304,11 +304,11 @@
                                 <span class="info-box-text">Uploads</span>
                                 <span class="info-box-number">13,648</span>
                             </div>
-                            <!-- /.info-box-content -->
+                            
                         </div>
-                        <!-- /.info-box -->
+                        
                     </div>
-                    <!-- /.col -->
+                    
                     <div class="col-md-3 col-sm-6 col-12">
                         <div class="info-box">
                             <span class="info-box-icon bg-danger"><i class="far fa-star"></i></span>
@@ -317,13 +317,13 @@
                                 <span class="info-box-text">Likes</span>
                                 <span class="info-box-number">93,139</span>
                             </div>
-                            <!-- /.info-box-content -->
+                            
                         </div>
-                        <!-- /.info-box -->
+                        
                     </div>
-                    <!-- /.col -->
+                    
                 </div>
-                <!-- /.row -->
+                
 
                 <!-- =========================================================== -->
 
@@ -345,11 +345,11 @@
                                     70% Increase in 30 Days
                                 </span>
                             </div>
-                            <!-- /.info-box-content -->
+                            
                         </div>
-                        <!-- /.info-box -->
+                        
                     </div>
-                    <!-- /.col -->
+                    
 
                     <div class="col-md-3 col-sm-6 col-12">
                         <div class="info-box bg-success">
@@ -366,11 +366,11 @@
                                     70% Increase in 30 Days
                                 </span>
                             </div>
-                            <!-- /.info-box-content -->
+                            
                         </div>
-                        <!-- /.info-box -->
+                        
                     </div>
-                    <!-- /.col -->
+                    
 
                     <div class="col-md-3 col-sm-6 col-12">
                         <div class="info-box bg-warning">
@@ -387,11 +387,11 @@
                                     70% Increase in 30 Days
                                 </span>
                             </div>
-                            <!-- /.info-box-content -->
+                            
                         </div>
-                        <!-- /.info-box -->
+                        
                     </div>
-                    <!-- /.col -->
+                    
 
                     <div class="col-md-3 col-sm-6 col-12">
                         <div class="info-box bg-danger">
@@ -408,18 +408,18 @@
                                     70% Increase in 30 Days
                                 </span>
                             </div>
-                            <!-- /.info-box-content -->
+                            
                         </div>
-                        <!-- /.info-box -->
+                        
                     </div>
-                    <!-- /.col -->
+                    
                     
                 </div>
-                <!-- /.row -->
+                
 
                 <!-- =========================================================== -->
 
-                <!-- Small Box (Stat card) -->
+                
                 <!-- <h5 class="mb-2 mt-4">Small Box</h5> -->
                 <div class="row">
                     <div class="col-lg-3 col-6">
@@ -438,9 +438,9 @@
                         </a>
                         </div>
                     </div>
-                    <!-- ./col -->
+                    
                     <div class="col-lg-3 col-6">
-                        <!-- small card -->
+                        
                         <div class="small-box bg-success">
                         <div class="inner">
                             <h3>53<sup style="font-size: 20px">%</sup></h3>
@@ -455,9 +455,9 @@
                         </a>
                         </div>
                     </div>
-                    <!-- ./col -->
+                    
                     <div class="col-lg-3 col-6">
-                        <!-- small card -->
+                        
                         <div class="small-box bg-warning">
                         <div class="inner">
                             <h3>44</h3>
@@ -472,9 +472,9 @@
                         </a>
                         </div>
                     </div>
-                    <!-- ./col -->
+                    
                     <div class="col-lg-3 col-6">
-                        <!-- small card -->
+                        
                         <div class="small-box bg-danger">
                         <div class="inner">
                             <h3>65</h3>
@@ -489,9 +489,9 @@
                         </a>
                         </div>
                     </div>
-                    <!-- ./col -->
+                    
                 </div>
-                <!-- /.row -->
+                
             </div>
         </section>
         
