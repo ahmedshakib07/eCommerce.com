adc5769f40254f65986905c774f35ba28226fc53
ahmedshakib <ahmedshakib37@gmail.com>
2024-03-12 13:59:15 +0600

    Database updated.

--- resources/views/admin/layouts/header.blade.php
+++ resources/views/admin/layouts/header.blade.php
@@ -199,7 +199,7 @@
 
                     <li class="nav-item">
                         <a href="{{ url('admin/shipping_charge/list') }}" class="nav-link @if(Request::segment(2) == 'shipping_charge') active @endif">
-                            <i class="nav-icon fas fa-receipt"></i>
+                            <i class="nav-icon fas fa-dolly"></i>
                             <p> Shipping Charge </p>
                         </a>
                     </li>
