f641f7995064428b39d3b702f8c35dce150a93aa
ahmedshakib <ahmedshakib37@gmail.com>
2024-03-21 15:50:15 +0600

    admin panel login background.

--- resources/views/admin/auth/login.blade.php
+++ resources/views/admin/auth/login.blade.php
@@ -14,68 +14,86 @@
   <!-- Theme style -->
   <link rel="stylesheet" href="{{ url('public/assets/dist/css/adminlte.min.css') }}">
 </head>
+
+<style>
+
+
+.login-page {
+  background-image: url("public/assets/dist/img/loginpage.jpg");
+
+  background-position: center;
+  background-size: cover;
+}
+
+</style>
+
 <body class="hold-transition login-page">
-<div class="login-box">
-  <div class="login-logo">
-    <a href="#"><b>e</b>Commerce.com</a>
-  </div>
-  <!-- /.login-logo -->
-  <div class="card">
-    <div class="card-body login-card-body">
-      <p class="login-box-msg">Sign in to start your session</p>
+  
+  <div class="login-box" style="width: 450px; margin-right: 550px;">
 
-      <form action="#" method="post">
+    <!-- /.login-logo -->
+    <div class="card">
+      <div class="login-logo">
+        <a href="#"><b>e</b>Commerce.com</a>
+      </div>
 
-        {{ csrf_field() }}
-        <div class="input-group mb-3">
-          <input type="email" class="form-control" placeholder="Email" name="email" required>
-          <div class="input-group-append">
-            <div class="input-group-text">
-              <span class="fas fa-envelope"></span>
+      <div class="card-body login-card-body">
+        <p class="login-box-msg">Sign in to start your session</p>
+
+        <form action="#" method="post">
+
+          {{ csrf_field() }}
+          <div class="input-group mb-3">
+            <input type="email" class="form-control" placeholder="Email" name="email" required>
+            <div class="input-group-append">
+              <div class="input-group-text">
+                <span class="fas fa-envelope"></span>
+              </div>
             </div>
           </div>
-        </div>
-        <div class="input-group mb-3">
-          <input type="password" class="form-control" placeholder="Password" name="password" required>
-          <div class="input-group-append">
-            <div class="input-group-text">
-              <span class="fas fa-lock"></span>
+          <div class="input-group mb-3">
+            <input type="password" class="form-control" placeholder="Password" name="password" required>
+            <div class="input-group-append">
+              <div class="input-group-text">
+                <span class="fas fa-lock"></span>
+              </div>
             </div>
           </div>
-        </div>
-        
-        <div class="row">
-          <div class="col-8">
-            <div class="icheck-primary">
-              <input type="checkbox" id="remember" name="remember">
-              <label for="remember">
-                Remember Me
-              </label>
+          
+          <div class="row">
+            <div class="col-8">
+              <div class="icheck-primary">
+                <input type="checkbox" id="remember" name="remember">
+                <label for="remember">
+                  Remember Me
+                </label>
+              </div>
             </div>
+            <!-- /.col -->
+            <div class="col-4">
+              <button type="submit" class="btn btn-primary btn-block">Sign In</button>
+            </div>
+            <!-- /.col -->
           </div>
-          <!-- /.col -->
-          <div class="col-4">
-            <button type="submit" class="btn btn-primary btn-block">Sign In</button>
-          </div>
-          <!-- /.col -->
-        </div>
 
-        <p class="mb-1">
-          <a href="#">I forgot my password</a>
-        </p>
+          <p class="mb-1">
+            <a href="#">I forgot my password</a>
+          </p>
 
-      </form>
+        </form>
+      </div>
+      <!-- /.login-card-body -->
+      
     </div>
-    <!-- /.login-card-body -->
   </div>
-</div>
-<!-- /.login-box -->
+  
+  <!-- /.login-box -->
 
-<!-- jQuery -->
-<script src="{{ url('public/assets/plugins/jquery/jquery.min.js') }}"></script>
-<!-- Bootstrap 4 -->
-<script src="{{ url('public/assets/plugins/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
-<!-- AdminLTE App -->
-<script src="{{ url('public/assets/dist/js/adminlte.min.js') }}"></script>
+  <!-- jQuery -->
+  <script src="{{ url('public/assets/plugins/jquery/jquery.min.js') }}"></script>
+  <!-- Bootstrap 4 -->
+  <script src="{{ url('public/assets/plugins/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+  <!-- AdminLTE App -->
+  <script src="{{ url('public/assets/dist/js/adminlte.min.js') }}"></script>
 </body>
 </html>
