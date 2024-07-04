2a63ac2f2c00ac5d755c9540f25a5dcaf5f86038
ahmedshakib <ahmedshakib37@gmail.com>
2024-07-04 16:45:29 +0600

    91 Admin Contact us List, Delete and Filters.

--- resources/views/admin/layouts/app.blade.php
+++ resources/views/admin/layouts/app.blade.php
@@ -54,5 +54,35 @@
             height: 100
         });
     </script>
+
+    <!-- For Sweet Alert Start -->
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+    <script>
+        $('.delete').on('click', function (e) {
+            e.preventDefault();
+            var self = $(this);
+            console.log(self.data('title'));
+
+            Swal.fire({
+                title: "Are you sure?",
+                text: "You won't be able to revert this!",
+                icon: "warning",
+                showCancelButton: true,
+                confirmButtonCouponCode: "#3085d6",
+                cancelButtonCouponCode: "#d33",
+                confirmButtonText: "Yes, delete it!"
+            }).then((result) => {
+                if (result.isConfirmed) {
+                    Swal.fire({
+                        title: "Deleted!",
+                        text: "Record has been deleted.",
+                        icon: "success"
+                    });
+                    location.href = self.attr('href');
+                }
+            });
+        })
+    </script>
+    <!-- For Sweet Alert End -->
 </body>
 </html>
--- resources/views/admin/subcategory/list.blade.php
+++ resources/views/admin/subcategory/list.blade.php
@@ -87,31 +87,5 @@
 @endsection
 
 @section('script')
-<script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
-<script>
-    $('.delete').on('click', function (e) {
-        e.preventDefault();
-        var self = $(this);
-        console.log(self.data('title'));
 
-        Swal.fire({
-            title: "Are you sure?",
-            text: "You won't be able to revert this!",
-            icon: "warning",
-            showCancelButton: true,
-            confirmButtonColor: "#3085d6",
-            cancelButtonColor: "#d33",
-            confirmButtonText: "Yes, delete it!"
-        }).then((result) => {
-            if (result.isConfirmed) {
-                Swal.fire({
-                    title: "Deleted!",
-                    text: "Sub Category has been deleted.",
-                    icon: "success"
-                });
-                location.href = self.attr('href');
-            }
-        });
-    })
-</script>
 @endsection
